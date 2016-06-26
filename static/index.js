b7b10db193fe959b3eae15deb5ddf4cc2e875818
clarencechen <clarencechenct@gmail.com>
2016-06-25 20:28:52 -0700

    mix up class and id

--- static/index.js
+++ static/index.js
@@ -1,8 +1,8 @@
 var host = location.origin.replace(/^http/, 'ws')
 var ws = new WebSocket(host);
 $(document).ready(function() {
-	$("button#submit").click(submitQuery);
-	$("#form-control").keypress(function(e){
+	$("button.submit").click(submitQuery);
+	$(".comment").keypress(function(e){
 		if(e.which == 13) {
 			e.preventDefault()
 			submitQuery()
@@ -12,17 +12,17 @@ $(document).ready(function() {
  		var obj = JSON.parse(event.data)
  		console.log(JSON.stringify(obj))
  		if(obj.error)
- 			$("p.chatbot").html(JSON.stringify(obj.error))
+ 			$("p#chatbot").html(JSON.stringify(obj.error))
  		else
- 			$("p.chatbot").html(JSON.stringify(obj))
+ 			$("p#chatbot").html(JSON.stringify(obj))
  	}
 })
 
 function submitQuery() {
-	if(("#form-control").val == "")
+	if(("#comment").val == "")
 		return;
-	var query = $("#form-control").val;
-	$("#form-control").val("");
+	var query = $("#comment").val;
+	$("#comment").val("");
 	ws.send("querytext::" + query);
 	console.log("emitted" + query);
 }
