7d21764fd1b13ed0abdf235dcbb93570b11a0f03
clarencechen <clarencechenct@gmail.com>
2016-06-25 19:09:11 -0700

    got rid of async

--- main.js
+++ main.js
@@ -54,29 +54,17 @@ setUpSocket()
 var jobID
 
 function callQuery(data, callback) {
-	client.post('analyzesentiment', data, true, function(err, resp) {
-		jobID = resp.body.jobID
-		console.log(jobID)
+	client.post('analyzesentiment', data, function(err, resp) {
+		if(err)
+		{
+			console.log('An error occured! ' + err)
+			bail(err, callback)
+		}
+		else
+		{
+			console.log('We got ' + JSON.stringify(resp.body))
+		}
 	})
-	
-
-	var responded = false
-	while(responded === false)
-	{
-		client.getJobStatus(jobID, function(err, resp) {
-			if(err)
-			{
-				console.log('An error occured! ' + err)
-				bail(err, callback)
-			}
-			else
-			{
-				console.log('We got ' + JSON.stringify(resp.body))
-				if(resp.body.aggregate)
-					responded = true;
-			}
-		})	
-	}
 }
 
 function bail(err, callback) {
