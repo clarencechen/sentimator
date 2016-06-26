c9aa5b840d9e6799a03ad41cabb8fc74096fa823
clarencechen <clarencechenct@gmail.com>
2016-06-25 18:00:24 -0700

    reorder websocket

--- static/index.js
+++ static/index.js
@@ -1,9 +1,11 @@
 var host = location.origin.replace(/^http/, 'ws')
 var ws = new WebSocket(host);
-
+var dummy = "This resteraunt\'s burgers smelled."
 $(document).ready(function() {
-	ws = new WebSocket(host);
-	ws.send("querytext::" + dummy)
+	
+	ws.onopen = function(){
+		ws.send("querytext::" + dummy)
+	}
 	ws.onmessage = function(event){
  		var obj = JSON.parse(event.data)
  		console.log(JSON.stringify(obj))
@@ -13,4 +15,3 @@ $(document).ready(function() {
  			$("#analysis").html(JSON.stringify(obj))
  	}
 })
-var dummy = "This resteraunt\'s burgers smelled."
